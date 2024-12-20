caf6a329ae714c526c7aa944c460e3d8dc4581d2
LeiO <2909735465@qq.com>
2024-12-20 20:30:12 +0800

    feat: 修改聊天发送图片/视频的服务器地址

--- src/controller/common/MediumController.ts
+++ src/controller/common/MediumController.ts
@@ -8,9 +8,21 @@ export class MediumController {
    * 上传图片/视频
    * @param file
    */
-  @Post('/', {middleware: [JwtMiddleware]})
+  @Post('/', { middleware: [JwtMiddleware] })
   async uploadMedium(@File() file: any) {
     const imgUrlArr = file.data?.split('/');
     return new ResultUtils().success('/' + imgUrlArr[imgUrlArr.length - 1]);
+  }
+  /**
+   * 聊天上传图片/视频
+   */
+  @Post('/chat', { middleware: [JwtMiddleware] })
+  async uploadChatMedium(@File() file: any) {
+    const imgUrlArr = file.data?.split('/');
+    const data = '/' + imgUrlArr[imgUrlArr.length - 1];
+    return new ResultUtils().success({
+      data,
+      mimeType: file.mimeType,
+    });
   }
 }
